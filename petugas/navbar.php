83b78666729d0806e8a94be86964bfc45416f130
Asus <deaaldiani@gmail.com>
2024-02-29 14:01:06 +0700

    ukk kasir

---
+++ petugas/navbar.php
@@ -0,0 +1,8 @@
+			<div class="card mt-5">
+				<div class="card-body">
+					<a href="index.php" class="btn btn-info" >Beranda</a>
+					<a href="data_barang.php" class="btn btn-info" >Data Barang</a>
+					<a href="pembelian.php" class="btn btn-info" >Pembelian</a>
+					<a href="../logout.php" class="btn btn-danger" >Keluar</a>
+				</div>
+			</div>
